4f696ff5fa871d9139014cc6a7359a6c19c601d3
timothy-breitenfeldt <timothy.breitenfeldt@smoothstack.com>
2020-02-01 20:42:47 -0500

    Initial commit.

---
+++ src/main/java/com/smoothstack/december/tokenserver/TokenserverMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.smoothstack.december.tokenserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TokenserverMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TokenserverMicroserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/smoothstack/december/tokenserver/TokenserverMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smoothstack.december.tokenserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TokenserverMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
